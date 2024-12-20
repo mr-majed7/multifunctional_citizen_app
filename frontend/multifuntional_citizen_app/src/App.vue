570e3ad76c01460a72f5bf6dd019bf98cfa28d19
mr_majed7 <majedulislam7789@gmail.com>
2024-12-20 21:49:24 +0600

    Add user ID and NID number to local storage on app mount

--- frontend/multifuntional_citizen_app/src/App.vue
+++ frontend/multifuntional_citizen_app/src/App.vue
@@ -13,6 +13,10 @@
 
 export default {
   components: {
+  },
+  mounted() {
+    localStorage.setItem('user_id', '123e4567-e89b-12d3-a456-426614174000');
+    localStorage.setItem('nid_number', '1234567890123');
   }
 }
 
