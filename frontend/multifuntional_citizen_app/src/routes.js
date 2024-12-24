7fa8634488227ab99ef9362855bd8c5d870b73ad
Shuvojit_paul <shuvojit.paul@g.bracu.ac.bd>
2024-12-24 08:55:39 +0600

    homepage

--- frontend/multifuntional_citizen_app/src/routes.js
+++ frontend/multifuntional_citizen_app/src/routes.js
@@ -1,6 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomePage from '@/components/HomePage.vue';
 
-const routes = createRouter({
+const routes = [
+  {
+    path: '/home',
+    name: 'home',
+    component: HomePage, 
+  },
+]
+const router = createRouter({
   history: createWebHistory(),
   routes: [],
 });
