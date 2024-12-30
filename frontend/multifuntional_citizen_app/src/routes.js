b723eec847e5fd61bd38129b33fae53788270fa7
mr_majed7 <majedulislam7789@gmail.com>
2024-12-31 00:43:46 +0600

    Add type hinting support files for various packages and create .env configuration

--- frontend/multifuntional_citizen_app/src/routes.js
+++ frontend/multifuntional_citizen_app/src/routes.js
@@ -4,29 +4,7 @@ import Manageprofile from "./components/Manageprofile.vue";
 import VoteSystem from "./components/VoteSystem.vue";
 import complaints from "./components/complaints.vue";
 
-import Listfiles from "@/components/Listfiles.vue";
-import TINRegistration from "@/components/TINRegistration.vue";
-import Upload from "@/components/Upload.vue";
-
-const routes = [
-  {
-    path: "/tinregistration",
-    name: "TINRegistration",
-    component: TINRegistration,
-  },
-  {
-    path: "/upload",
-    name: "Upload",
-    component: Upload,
-  },
-  {
-    path: "/listfiles",
-    name: "Listfiles",
-    component: Listfiles,
-  },
-];
-
-const router = createRouter({
+const routes = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/votesystem", component: VoteSystem },
@@ -34,7 +12,6 @@ const router = createRouter({
     { path: "/emergencycon", component: EmergencyCon },
     { path: "/complaints", component: complaints },
   ],
-  routes: routes,
 });
 
 export default router;
